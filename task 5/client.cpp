4a460e334ec57b549640013e9ecd126c60eaea19
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2023-12-06 11:15:17 +0300

    Update client.cpp

--- task 5/client.cpp
+++ task 5/client.cpp
@@ -1,71 +1,52 @@
 #include <iostream>
+#include <cstdlib>
 #include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
 
+// Структура для передачи данных от клиента к серверу
 struct StudentData {
-    char lastName[50];
-    int grades[5];
+    char lastName[256];
+    int grades[4];
 };
 
 int main() {
-    // Создаем сокет
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket == -1) {
-        std::cerr << "Error in socket creation.\n";
-        return -1;
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(12345);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // IP-адрес сервера
+
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
+        std::cerr << "Error connecting to the server.\n";
+        return 1;
     }
-
-    // Настраиваем адрес сервера
-    sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(8080);
-    inet_pton(AF_INET, "127.0.0.1", &(server_address.sin_addr));
-
-    // Подключаемся к серверу
-    if (connect(client_socket, reinterpret_cast<struct sockaddr*>(&server_address), sizeof(server_address)) == -1) {
-        std::cerr << "Error in connection.\n";
-        close(client_socket);
-        return -1;
-    }
-
-    std::cout << "Connected to the server. Type 'quit' to exit.\n";
 
     while (true) {
-        StudentData studentData;
+        StudentData student;
 
-        // Вводим данные студента
-        std::cout << "Enter last name: ";
-        std::cin.getline(studentData.lastName, sizeof(studentData.lastName));
+        std::cout << "Enter last name (or 'quit' to exit): ";
+        std::cin >> student.lastName;
 
-        std::cout << "Enter grades (5 numbers separated by space): ";
-        for (int i = 0; i < 5; ++i) {
-            std::cin >> studentData.grades[i];
-        }
-
-        // Отправляем данные на сервер
-        send(client_socket, &studentData, sizeof(StudentData), 0);
-
-        // Проверяем, если введена команда "quit", то закрываем соединение
-        if (strncmp(studentData.lastName, "quit", 4) == 0) {
-            std::cout << "Closing the connection.\n";
+        if (strcmp(student.lastName, "quit") == 0) {
+            send(clientSocket, &student, sizeof(StudentData), 0);
             break;
         }
 
-        // Получаем ответ от сервера
+        std::cout << "Enter 4 grades: ";
+        for (int i = 0; i < 4; ++i) {
+            std::cin >> student.grades[i];
+        }
+
+        send(clientSocket, &student, sizeof(StudentData), 0);
+
         char buffer[1024];
-        int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-        if (bytes_received <= 0) {
-            std::cerr << "Error in receiving data or connection closed.\n";
-            break;
-        }
+        recv(clientSocket, buffer, sizeof(buffer), 0);
 
-        // Выводим ответ от сервера
-        std::cout << "Server response: " << std::string(buffer) << "\n";
+        std::cout << "Server response: " << buffer << std::endl;
     }
 
-    // Закрываем клиентский сокет
-    close(client_socket);
-
+    close(clientSocket);
     return 0;
 }
