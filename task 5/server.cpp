409c5264884f36ea9fcdab257aee351bd9424802
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2023-12-06 11:14:59 +0300

    Update server.cpp

--- task 5/server.cpp
+++ task 5/server.cpp
@@ -1,101 +1,113 @@
 #include <iostream>
-#include <thread>
+#include <cstdlib>
 #include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+#include <algorithm>
 
+// Структура для передачи данных от клиента к серверу
 struct StudentData {
-    char lastName[50];
-    int grades[5];
+    char lastName[256];
+    int grades[4];
 };
 
-void handle_client(int client_socket) {
-    StudentData studentData;
-    int bytes_received;
+// Функция для вычисления стипендии
+std::string calculateScholarship(const StudentData& student) {
+    bool hasDebt = false;
+    bool hasThree = false;
 
-    while (true) {
-        // Получаем данные от клиента
-        bytes_received = recv(client_socket, &studentData, sizeof(StudentData), 0);
-        if (bytes_received <= 0) {
-            std::cerr << "Error in receiving data or connection closed.\n";
+    for (int i = 0; i < 4; ++i) {
+        if (student.grades[i] == 2) {
+            hasDebt = true;
             break;
+        } else if (student.grades[i] == 3) {
+            hasThree = true;
         }
-
-        // Проверяем, если клиент отправил "quit", то разрываем соединение
-        if (strncmp(studentData.lastName, "quit", 4) == 0) {
-            std::cout << "Client requested to close the connection. Closing...\n";
-            break;
-        }
-
-        // Вычисляем средний балл
-        double averageGrade = 0.0;
-        for (int i = 0; i < 5; ++i) {
-            averageGrade += studentData.grades[i];
-        }
-        averageGrade /= 5.0;
-
-        // Определяем стипендию
-        std::string scholarship;
-        if (averageGrade >= 4.5) {
-            scholarship = "Full scholarship";
-        } else if (averageGrade >= 4.0) {
-            scholarship = "Partial scholarship";
-        } else {
-            scholarship = "No scholarship";
-        }
-
-        // Отправляем результат обратно клиенту
-        send(client_socket, scholarship.c_str(), scholarship.length() + 1, 0);
     }
 
-    // Закрываем соединение с клиентом
-    close(client_socket);
+    if (hasDebt) {
+        return "No scholarship due to debt.";
+    } else if (hasThree) {
+        return "No scholarship due to grade 3.";
+    } else if (std::all_of(student.grades, student.grades + 4, [](int grade) { return grade == 5; })) {
+        return "Increased scholarship.";
+    } else {
+        return "Regular scholarship.";
+    }
+}
+
+// Функция для обработки запросов клиента
+void* handleClient(void* arg) {
+    int clientSocket = *((int*)arg);
+    delete (int*)arg;
+
+    std::cout << "Client connected.\n";
+
+    while (true) {
+        StudentData student;
+        int bytesRead = recv(clientSocket, &student, sizeof(StudentData), 0);
+
+        if (bytesRead <= 0) {
+            std::cout << "Connection closed by client.\n";
+            break;
+        }
+
+        if (strcmp(student.lastName, "quit") == 0) {
+            std::cout << "Client requested to quit. Closing connection.\n";
+            break;
+        }
+
+        // Проверка корректности данных оценок
+        bool validGrades = true;
+        for (int i = 0; i < 4; ++i) {
+            if (student.grades[i] < 2 || student.grades[i] > 5) {
+                validGrades = false;
+                break;
+            }
+        }
+
+        if(validGrades==false) {
+            std::string response;
+
+            if (!validGrades) {
+                response = "Invalid grades. Grades must be in the range of 2 to 5.";
+            } else {
+                response = calculateScholarship(student);
+            }
+
+            send(clientSocket, response.c_str(), response.size() + 1, 0);
+        }
+        std::string scholarship = calculateScholarship(student);
+        send(clientSocket, scholarship.c_str(), scholarship.size() + 1, 0);
+    }
+
+    close(clientSocket);
+    return NULL;
 }
 
 int main() {
-    // Создаем сокет
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1) {
-        std::cerr << "Error in socket creation.\n";
-        return -1;
-    }
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Настраиваем адрес сервера
-    sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = INADDR_ANY;
-    server_address.sin_port = htons(8080);
+    sockaddr_in serverAddr{};
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(12345);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
 
-    // Связываем сокет с адресом сервера
-    if (bind(server_socket, reinterpret_cast<struct sockaddr*>(&server_address), sizeof(server_address)) == -1) {
-        std::cerr << "Error in binding.\n";
-        close(server_socket);
-        return -1;
-    }
+    bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+    listen(serverSocket, 5);
 
-    // Начинаем слушать запросы
-    if (listen(server_socket, 5) == -1) {
-        std::cerr << "Error in listening.\n";
-        close(server_socket);
-        return -1;
-    }
-
-    std::cout << "Server listening on port 8080...\n";
+    std::cout << "Server listening on port 12345...\n";
 
     while (true) {
-        // Принимаем новое соединение
-        int client_socket = accept(server_socket, nullptr, nullptr);
-        if (client_socket == -1) {
-            std::cerr << "Error in accepting connection.\n";
-            continue;
-        }
+        int* clientSocket = new int;
+        *clientSocket = accept(serverSocket, NULL, NULL);
 
-        // Запускаем новый поток для обработки клиента
-        std::thread(handle_client, client_socket).detach();
+        pthread_t thread;
+        pthread_create(&thread, NULL, handleClient, (void*)clientSocket);
+        pthread_detach(thread);
     }
 
-    // Закрываем серверный сокет
-    close(server_socket);
-
+    close(serverSocket);
     return 0;
 }
